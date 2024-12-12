e1fef39b621476635b08d2f987505b7d424b0f76
Nischit007 <nischitpantha@gmail.com>
2024-12-12 11:23:59 +0545

    Update Login.jsx

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -21,7 +21,7 @@ const Login = () => {
     console.log(JSON.stringify(formData));
     
   try {
-    const response = await fetch('http://localhost:8000/auth/login', {
+    const response = await fetch('map-backend-eight.vercel.app/auth/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(formData),
