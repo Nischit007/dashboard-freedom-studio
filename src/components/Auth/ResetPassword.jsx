c509e639d7f38f333e5f2e78b7c79f4719812ad2
Nischit007 <nischitpantha@gmail.com>
2024-12-12 11:24:31 +0545

    Update ResetPassword.jsx

--- src/components/Auth/ResetPassword.jsx
+++ src/components/Auth/ResetPassword.jsx
@@ -27,7 +27,7 @@ const ResetPassword = () => {
     }
 
     try {
-      const response = await fetch('http://localhost:8000/auth/reset-password', {
+      const response = await fetch('map-backend-eight.vercel.app/auth/reset-password', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
