c72dab2e84a96254bb798cec2440d8dfbe26c6db
Nischit007 <nischitpantha@gmail.com>
2024-12-12 11:18:32 +0545

    Update Table.jsx

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -6,7 +6,7 @@ const Table = () => {
   const [jwt, setJwt] = useState(() => localStorage.getItem('jwt'));
   useEffect(() => {
     // Fetch data from the API
-    axios.get('http://localhost:8000/api/locations',{
+    axios.get('map-backend-eight.vercel.app/api/locations',{
       headers: {        
         Authorization: `Bearer ${jwt}`,
       },
@@ -29,7 +29,7 @@ const Table = () => {
 
   const handleDelete = (id) => {
     console.log(`Attempting to delete item with id: ${id}`);
-    axios.delete(`http://localhost:8000/api/locations/${id}`,{
+    axios.delete(`map-backend-eight.vercel.app/locations/${id}`,{
       headers: {        
         Authorization: `Bearer ${jwt}`,
       },
@@ -50,7 +50,7 @@ const Table = () => {
   const getImageUrl = (fileName) => {
     console.log("-----",fileName);
     
-    return fileName ? `http://localhost:8000/uploads/${fileName.replace('\\', '/')}` : 'https://via.placeholder.com/150';
+    return fileName ? `map-backend-eight.vercel.app/uploads/${fileName.replace('\\', '/')}` : 'https://via.placeholder.com/150';
   };
 
   return (
